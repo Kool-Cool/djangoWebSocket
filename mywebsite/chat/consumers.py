e909bccad65782a754eb4b389da0c9690c8193e7
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2024-08-09 13:25:25 +0530

    client server interaction
    Added sync async
    
    Handled the closing of Handshake

--- mywebsite/chat/consumers.py
+++ mywebsite/chat/consumers.py
@@ -1,32 +1,44 @@
-#channels version of view 
-# response to  request from client and initate while keeping open 
-
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
+    
     def connect(self):
+        self.room_group_name = 'test'
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
         self.send(text_data=json.dumps({
             'type': 'connection_established',
             'message': 'You are now Connected!',
         }))
 
-
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        print("Message from WEB" , message)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message
+            }
+        )
 
-        self.send(text_data = json.dumps({
-            "type": "chat",
-            "message" : message ,
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
         }))
 
-
-
-
-    # def disconnect(self, close_code):
-    #     pass
-
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -47,6 +47,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'mywebsite.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
